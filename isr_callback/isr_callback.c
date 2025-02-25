25700b0a3bd66ae18e6139822713e12f44b1da13
Deep Bhuinya <deepb.cse@gmail.com>
2025-02-25 10:40:46 +0530

    Removed the mtech project and modified the isr_callback

--- isr_callback/isr_callback.c
+++ isr_callback/isr_callback.c
@@ -1,22 +1,37 @@
 /*
+Copyright (c) 2025 Deep Bhuinya
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+software and associated documentation files (the "Software"), to deal in the Software 
+without restriction, including without limitation the rights to use, copy, modify, merge, 
+publish, distribute, sublicense, and/or sell copies of the Software, and to permit 
+persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies 
+or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+DEALINGS IN THE SOFTWARE.
+
+Author:         Deep Bhuinya
+Email:          deepb.cse@gmail.com
+*/
+
+/*
+Question:
 Design and implement an API that allows multiple clients to register for notifications
 when new data from any generic sensor (temperature, GPS, accelerometer, etc.) is available.
-*/
 
-/*
-Interrupt flow:
-1. GPIO hardware inside the microcontroller continuously monitors the voltage level of the pin.
-2. When the voltage changes (HIGH to LOW or LOW to HIGH), the GPIO detects the event.
-3. The NVIC (Interrupt Controller inside the microcontroller) triggers an interrupt request (IRQ).
-4. The CPU then executes the corresponding ISR (Interrupt Service Routine).
-*/
-
-/*
-Design considerations:
-- Thread-Safety
-- Callback Mechanism
-- Memory-Constrained Systems
-- Low-Power Optimization
+Features:
+- Multiple sensors
+- Callback register mechanism
+- Threaded Callbacks
+- Memory-Constrained systems
+- Low-Power optimization
 */
 
 #include <stdio.h>
@@ -24,163 +39,209 @@ Design considerations:
 #include <unistd.h>
 #include <pthread.h>
 #include <stdint.h>
+#include <string.h>
 
 #define MAX_ALLOWED_CALLBACKS 10
-#define TEMP_SENSOR 0  // Bit 0 in the GPIO_PIN_REG
-#define ACC_SENSOR 1
-#define GPS_SENSOR 2
 
-volatile uint8_t GPIO_PIN_REG = 0x00;
+typedef void (*PointerToFunc)(void *);
 
-typedef void (*pointer_to_func)(void *);
-
-typedef struct callback_register_book {
-    pointer_to_func callback[MAX_ALLOWED_CALLBACKS];
+// Structure to store registered callbacks
+typedef struct {
+    PointerToFunc callback[MAX_ALLOWED_CALLBACKS];
     char sensor_type_for_callback[MAX_ALLOWED_CALLBACKS];
     int num_of_callbacks;
     pthread_mutex_t lock;
-} callback_register_book;
+} CallbackRegisterBook;
 
-typedef struct sensor_data {
+// Structure to store sensor data
+typedef struct {
     float temp_sensor_data;
     int acc_sensor_data[3];
     char gps_sensor_data[50];
-} sensor_data;
+} SensorData;
 
-void callback_register_book_init(callback_register_book *callback_booklet) {
-    for (int i = 0; i < MAX_ALLOWED_CALLBACKS; i++) {
-        callback_booklet -> callback[i] = NULL;
-        callback_booklet -> sensor_type_for_callback[i] = 0;
-    }
-    callback_booklet -> num_of_callbacks = 0;
-    pthread_mutex_init(&(callback_booklet->lock), NULL);
-}
+// Bitfield structure for GPIO register
+typedef struct {
+    uint8_t temp    : 1;
+    uint8_t acc     : 1;
+    uint8_t gps     : 1;
+    uint8_t rsvd    : 5;
+} GpioPinReg;
 
-void sensor_data_init(sensor_data *sensor_data) {
-    sensor_data -> temp_sensor_data = 0.0;
-    for (int i = 0; i < 3; i++) {
-        sensor_data -> acc_sensor_data[i] = 0;
-    }
-    for (int i = 0; i < 50; i++) {
-        sensor_data -> gps_sensor_data[i] = 0;
-    }
-}
-
-void Client1CallbackMethod(void *temp_sensor_data) {  
-    printf("Client 1: Received Temperature Sensor Data: %.2f\n", *((float *)temp_sensor_data));
-}
-void Client2CallbackMethod(void *acc_sensor_data) {
-    printf("Client 2: Received Accelerometer Sensor Data: x = %d, y = %d, z = %d\n", ((int *)acc_sensor_data)[0], ((int *)acc_sensor_data)[1], ((int *)acc_sensor_data)[2]);
-}
-
-void Client3CallbackMethod(void *gps_sensor_data) {
-    printf("Client 3: Received GPS Sensor Data: ");
-    puts((char *)gps_sensor_data);
-}
-
-int CallbackRegister(callback_register_book *callback_booklet, pointer_to_func ClientCallbackMethod, char sensor) {
-    pthread_mutex_lock(&(callback_booklet->lock));
-    if (callback_booklet -> num_of_callbacks > MAX_ALLOWED_CALLBACKS) {
-        pthread_mutex_unlock(&(callback_booklet -> lock));
-        return -1;
-    }
-    callback_booklet -> callback[callback_booklet -> num_of_callbacks] = ClientCallbackMethod;
-    callback_booklet -> sensor_type_for_callback[callback_booklet -> num_of_callbacks] = sensor;
-    (callback_booklet -> num_of_callbacks)++;
-    pthread_mutex_unlock(&(callback_booklet->lock));
-    return 0;
-}
-
-void ReadTempSensorData(float *temp_sensor_data) {
-    *temp_sensor_data = 25.67;
-}
-
-void ReadAccSensorData(int *acc_sensor_data) {
-    acc_sensor_data[0] = 1;
-    acc_sensor_data[1] = 2;
-    acc_sensor_data[2] = 3;
-}
-
-void ReadGpsSensorData(char *gps_sensor_data) {
-    sprintf(gps_sensor_data, "Latitude: 12.34, Longitude: 56.78");
-}
-
-void CallbackToRegisteredClients(callback_register_book *callback_booklet, sensor_data *sensor_data) {
-    for (int i = 0; i < callback_booklet -> num_of_callbacks; i++) {
-        switch(callback_booklet -> sensor_type_for_callback[i]) {
-            case 'T':
-                callback_booklet -> callback[i](&(sensor_data -> temp_sensor_data));
-                break;
-            case 'A':
-                callback_booklet -> callback[i](sensor_data -> acc_sensor_data);
-                break;
-            case 'G':
-                callback_booklet -> callback[i](sensor_data -> gps_sensor_data);
-                break;
-        }
-    }
-}
-
-void GPIO_pin_set(char sensor) {  // Multiple Sensor, thread safety?
-    switch(sensor) {
-        case 'T':
-            GPIO_PIN_REG |= (1 < TEMP_SENSOR);
-            break;
-        case 'A':
-            GPIO_PIN_REG |= (1 < ACC_SENSOR);
-            break;
-        case 'G':
-            GPIO_PIN_REG |= (1 < GPS_SENSOR);
-            break;
-    }
-}
-
-void Sensor_IRQHandler(char sensor, sensor_data *sensor_data) {  // Multiple Sensor, thread safety?
-    switch(sensor) {
-        case 'T':
-            GPIO_PIN_REG &= ~(1 < TEMP_SENSOR);
-            ReadTempSensorData(&(sensor_data -> temp_sensor_data));
-            break;
-        case 'A':
-            GPIO_PIN_REG &= ~(1 < ACC_SENSOR);
-            ReadAccSensorData(sensor_data -> acc_sensor_data);
-            break;
-        case 'G':
-            GPIO_PIN_REG &= ~(1 < GPS_SENSOR);
-            ReadGpsSensorData(sensor_data -> gps_sensor_data);
-            break;
-    }
-}
+// Function Prototypes
+void callbackRegisterBookInit(CallbackRegisterBook *);
+void sensorDataInit(SensorData *);
+void gpioPinRegInit(GpioPinReg *);
+int callbackRegister(CallbackRegisterBook *, PointerToFunc, char);
+void client1CallbackMethod(void *);
+void client2CallbackMethod(void *);
+void client3CallbackMethod(void *);
+void gpioPinSet(GpioPinReg *, char);
+void sensorIrqHandler(GpioPinReg *, char, SensorData *);
+void readTempSensorData(float *);
+void readAccSensorData(int *);
+void readGpsSensorData(char *);
+void callbackToRegisteredClients(CallbackRegisterBook *, SensorData *);
+void *callbackThread(void *arg);
 
 int main() {
-    // Create and Initializing the Data Structures
-    callback_register_book callback_booklet;
-    callback_register_book_init(&callback_booklet);
-    sensor_data sensor_data;
-    sensor_data_init(&sensor_data);
+    printf("\n======== Sensor Notification System Started ========\n\n");
+    CallbackRegisterBook callback_booklet;
+    SensorData sensor_data;
+    GpioPinReg gpio_reg;
 
-    // Clients registering themselves
-    CallbackRegister(&callback_booklet, Client1CallbackMethod, 'T');
-    CallbackRegister(&callback_booklet, Client2CallbackMethod, 'A');
-    CallbackRegister(&callback_booklet, Client3CallbackMethod, 'G');
+    callbackRegisterBookInit(&callback_booklet);
+    sensorDataInit(&sensor_data);
+    gpioPinRegInit(&gpio_reg);
 
-    // Raising Interrupt when data is ready
-    GPIO_pin_set('T');  // Sensor sets the GPIO pins when the data is ready
-    Sensor_IRQHandler('T', &sensor_data);  // Interrupt Controller raising the interrupt after GPIO detects the pin change.
+    // Registering Clients
+    callbackRegister(&callback_booklet, client1CallbackMethod, 'T');
+    callbackRegister(&callback_booklet, client2CallbackMethod, 'A');
+    callbackRegister(&callback_booklet, client3CallbackMethod, 'G');
 
-    sleep(1);  // Simulating the time in the data availability
-    GPIO_pin_set('A');
-    Sensor_IRQHandler('A', &sensor_data);
+    // Trigger interrupt when sensor data is ready
+    sleep(1);  // Simulating data ready time
+    printf("\n[SENSOR T] Data ready\n");
+    gpioPinSet(&gpio_reg, 'T');
+    printf("[INTERRUPT CONTROLLER] Triggering interrupt for sensor: T\n");
+    sensorIrqHandler(&gpio_reg, 'T', &sensor_data);
 
     sleep(1);
-    GPIO_pin_set('G');
-    Sensor_IRQHandler('G', &sensor_data);
+    printf("[SENSOR A] Data ready\n");
+    gpioPinSet(&gpio_reg, 'A');
+    printf("[INTERRUPT CONTROLLER] Triggering interrupt for sensor: A\n");
+    sensorIrqHandler(&gpio_reg, 'A', &sensor_data);
 
-    // Registered clients getting callback by a task scheduler
-    CallbackToRegisteredClients(&callback_booklet, &sensor_data);
+    sleep(1);
+    printf("[SENSOR G] Data ready\n");
+    gpioPinSet(&gpio_reg, 'G');
+    printf("[INTERRUPT CONTROLLER] Triggering interrupt for sensor: G\n");
+    sensorIrqHandler(&gpio_reg, 'G', &sensor_data);
+
+    // Notify registered clients
+    printf("[TASK SCHEDULER] Will trigger callback to registered clients shortly...\n\n");
+    sleep(2);  // Simulating a time based task scheduler.
+    callbackToRegisteredClients(&callback_booklet, &sensor_data);
 
     // Destroy the mutexes
     pthread_mutex_destroy(&(callback_booklet.lock));
 
+    printf("\n======== Sensor Notification System Completed ========\n\n");
+
     return 0;
+}
+
+// Initialize callback registry
+void callbackRegisterBookInit(CallbackRegisterBook *callback_booklet) {
+    memset(callback_booklet->callback, 0, sizeof(callback_booklet->callback));
+    memset(callback_booklet->sensor_type_for_callback, 0, sizeof(callback_booklet->sensor_type_for_callback));
+    callback_booklet->num_of_callbacks = 0;
+    pthread_mutex_init(&(callback_booklet->lock), NULL);
+}
+
+// Initialize sensor data
+void sensorDataInit(SensorData *sensor_data) {
+    sensor_data->temp_sensor_data = 0.0;
+    memset(sensor_data->acc_sensor_data, 0, sizeof(sensor_data->acc_sensor_data));
+    memset(sensor_data->gps_sensor_data, 0, sizeof(sensor_data->gps_sensor_data));
+}
+
+// Initialize GPIO register
+void gpioPinRegInit(GpioPinReg *gpio_reg) {
+    memset(gpio_reg, 0, sizeof(*gpio_reg));
+}
+
+// Register a client for sensor callbacks
+int callbackRegister(CallbackRegisterBook *callback_booklet, PointerToFunc clientCallbackMethod, char sensor) {
+    pthread_mutex_lock(&(callback_booklet->lock));
+
+    if (callback_booklet->num_of_callbacks >= MAX_ALLOWED_CALLBACKS) {
+        pthread_mutex_unlock(&(callback_booklet->lock));
+        return -1;
+    }
+
+    int index = callback_booklet->num_of_callbacks;
+
+    callback_booklet->callback[index] = clientCallbackMethod;
+    callback_booklet->sensor_type_for_callback[index] = sensor;
+    callback_booklet->num_of_callbacks++;
+    printf("[Client %d] Registered callback for sensor: %c\n", index + 1, sensor);
+
+    pthread_mutex_unlock(&(callback_booklet->lock));
+    return 0;
+}
+
+// Simulated client callback methods
+void client1CallbackMethod(void *temp_sensor_data) {  
+    printf("[Client 1] Received callback for sensor: T, data: %.2f\n", *((float *)temp_sensor_data));
+}
+
+void client2CallbackMethod(void *acc_sensor_data) {
+    int *data = (int *)acc_sensor_data;
+    printf("[Client 2] Received callback for sensor: A, Data: x = %d, y = %d, z = %d\n", data[0], data[1], data[2]);
+}
+
+void client3CallbackMethod(void *gps_sensor_data) {
+    printf("[Client 3] Received callback for sensor: G, Data: %s\n", (char *)gps_sensor_data);
+}
+
+// Simulate setting a GPIO pin when sensor data is ready
+void gpioPinSet(GpioPinReg *gpio_reg, char sensor) {
+    switch(sensor) {
+        case 'T': gpio_reg->temp = 1; break;
+        case 'A': gpio_reg->acc = 1; break;
+        case 'G': gpio_reg->gps = 1; break;
+    }
+}
+
+// Interrupt Service Routine (ISR) to read sensor data
+void sensorIrqHandler(GpioPinReg *gpio_reg, char sensor, SensorData *sensor_data) {
+    switch(sensor) {
+        case 'T': gpio_reg->temp = 0; readTempSensorData(&(sensor_data->temp_sensor_data)); break;
+        case 'A': gpio_reg->acc = 0; readAccSensorData(sensor_data->acc_sensor_data); break;
+        case 'G': gpio_reg->gps = 0; readGpsSensorData(sensor_data->gps_sensor_data); break;
+    }
+    
+    printf("[MCU] ISR done for sensor: %c\n\n", sensor);
+}
+
+// Simulated sensor data reads
+void readTempSensorData(float *temp_sensor_data) { *temp_sensor_data = 25.67; }
+void readAccSensorData(int *acc_sensor_data) { acc_sensor_data[0] = 1; acc_sensor_data[1] = 2; acc_sensor_data[2] = 3; }
+void readGpsSensorData(char *gps_sensor_data) { sprintf(gps_sensor_data, "Lat: 12.34, Long: 56.78"); }
+
+// Notify registered clients using threads
+void callbackToRegisteredClients(CallbackRegisterBook *callback_booklet, SensorData *sensor_data) {
+    if (callback_booklet->num_of_callbacks == 0) {
+        return; 
+    }
+
+    pthread_t threads[callback_booklet->num_of_callbacks];
+
+    for (int i = 0; i < callback_booklet->num_of_callbacks; i++) {
+        void *data = NULL;
+        switch(callback_booklet->sensor_type_for_callback[i]) {
+            case 'T': data = &(sensor_data->temp_sensor_data); break;
+            case 'A': data = sensor_data->acc_sensor_data; break;
+            case 'G': data = sensor_data->gps_sensor_data; break;
+        }
+        if (data) {
+            void **args = malloc(2 * sizeof(void *));
+            args[0] = callback_booklet->callback[i];
+            args[1] = data;
+            pthread_create(&threads[i], NULL, callbackThread, args);
+        }
+    }
+    for (int i = 0; i < callback_booklet->num_of_callbacks; i++) {
+        pthread_join(threads[i], NULL);
+    }
+}
+
+// Thread function to handle callbacks
+void *callbackThread(void *arg) {
+    PointerToFunc callback = ((PointerToFunc *)arg)[0];
+    void *data = ((void **)arg)[1];
+    callback(data);
+    free(arg);
+    return NULL;
 }
